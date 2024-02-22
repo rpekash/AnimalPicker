dcfc7329086101504c5403aa74cdc36b44472b45
rpekash <ksyla@quinnipiac.edu>
2024-02-21 19:43:31 -0500

    Step Three

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class AnimalPicker{
@@ -7,17 +8,28 @@ public class AnimalPicker{
         System.out.println("Welcome!");
         System.out.println("Would you like to see: 1. a dog 2. a cat.");
         System.out.println("Please select the number of the animal you would like to see.");
+        int selection = -1;
 
+do{
+    try {
         int userInput = scanner.nextInt();
 
         if(userInput == 1){
+            selection = userInput - 1;
             System.out.println("DOG ASCII ART HERE");
         }
         else if(userInput == 2){
+            selection = userInput - 1;
             System.out.println("CAT ASCII ART HERE");
         }
         else {
             System.out.println("Error: That number does not correspond with an animal, please try again.");
         }
+    }
+            catch(InputMismatchException e) {
+                System.out.println("Error: That is not a valid number, please try again.");
+                scanner.nextLine();
+            }
+        } while(selection<0);
     }
 }
